73a0c438a17e5f413e3b5173e97bd52143c98251
Sedulla <67521243+Sedulla@users.noreply.github.com>
2021-09-09 19:10:57 +0400

    Initial commit

---
+++ assets/js/onscroll.js
@@ -0,0 +1,17 @@
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  let navbar = document.getElementById('navbar');
+
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    navbar.classList.add('shadow');
+    navbar.classList.add('bg-white');
+    navbar.classList.remove('mt-3');
+  } else {
+    navbar.classList.remove('shadow');
+    navbar.classList.remove('bg-white');
+    navbar.classList.add('mt-3');
+  }
+}
